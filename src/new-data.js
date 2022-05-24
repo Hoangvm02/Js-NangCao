e116ce85d4c806abb6b4f48bc6ce8936064cdbc7
Hoangvm02 <hoangvmph13792@fpt.edu.vn>
2022-05-24 17:18:43 +0700

    blog

---
+++ src/new-data.js
@@ -0,0 +1,18 @@
+const news = [
+    {
+        id: 1,
+        title: 'Transactions',
+        content: 'Find the latest events updates or create events, concerts, conferences, workshops, exhibitions, and cultural events in all cities of the US. The aim of Eventistan is to promote healthy and entertaining event.Find the latest events updates or create events, concerts, conferences, workshops, exhibitions, and cultural events in all cities of the US. The aim of Eventistan is to promote healthy and entertaining event.'
+    },
+    {
+        id: 2,
+        title: 'Transactions',
+        content: 'Find the latest events updates or create events, concerts, conferences, workshops, exhibitions, and cultural events in all cities of the US. The aim of Eventistan is to promote healthy and entertaining event.Find the latest events updates or create events, concerts, conferences, workshops, exhibitions, and cultural events in all cities of the US. The aim of Eventistan is to promote healthy and entertaining event.'
+    },
+    {
+        id: 3,
+        title: 'Transactions',
+        content: 'Find the latest events updates or create events, concerts, conferences, workshops, exhibitions, and cultural events in all cities of the US. The aim of Eventistan is to promote healthy and entertaining event.Find the latest events updates or create events, concerts, conferences, workshops, exhibitions, and cultural events in all cities of the US. The aim of Eventistan is to promote healthy and entertaining event.'
+    },
+];
+export default news
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,51 +1,90 @@
+import tintuc  from "../new-data"
 const About = {
     render : () => `
         
-    <div class="2xl:container 2xl:mx-auto lg:py-16 lg:px-20 md:py-12 md:px-6 py-9 px-4">
-    <div class="flex flex-col lg:flex-row justify-between gap-8">
-        <div class="w-full lg:w-5/12 flex flex-col justify-center">
-            <h1 class="text-3xl lg:text-4xl font-bold leading-9 text-gray-800 dark:text-white pb-4">About Us</h1>
-            <p class="font-normal text-base leading-6 text-gray-600 dark:text-white">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum.In the first place we have granted to God, and by this our present charter confirmed for us and our heirs forever that the English Church shall be free, and shall have her rights entire, and her liberties inviolate; and we will that it be thus observed; which is apparent from</p>
-        </div>
-        <div class="w-full lg:w-8/12">
-            <img class="w-full h-full" src="https://i.ibb.co/FhgPJt8/Rectangle-116.png" alt="A group of People" />
-        </div>
-    </div>
+    <div id="blog" class="bg-gray-100 dark:bg-gray-900 px-4 xl:px-4 py-14">
+    ${tintuc.map((tin) => (
+    `<div>
+    <div class="mx-auto container">
+        <div tabindex="0"  aria-label="Group of cards" class="focus:outline-none mt-12 lg:mt-24">
+            <div  class="grid sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-2 gap-8">
+                <div tabindex="0" class="focus:outline-none" aria-label="card 1">
+                    <img role="img" aria-label="code editor" tabindex="0" class="focus:outline-none w-full" src="https://cdn.tuk.dev/assets/components/111220/Blg-6/blog(1).png" alt="code editor" />
+                    <div class="py-4 px-8 w-full flex justify-between bg-indigo-700">
+                        <p  tabindex="0" class="focus:outline-none text-sm text-white font-semibold tracking-wide">Bruce Wayne</p>
+                        <p tabindex="0" class="focus:outline-none text-sm text-white font-semibold tracking-wide">13TH Oct, 2020</p>
+                    </div>
+                    <div class="bg-white dark:bg-gray-800 px-10 py-6 rounded-bl-3xl rounded-br-3xl">
+                   
+                        <h1 tabindex="0" class="focus:outline-none text-4xl text-gray-900 dark:text-white font-semibold tracking-wider">${tin.title}</h1>
+                        <p tabindex="0" class="focus:outline-none text-gray-700 dark:text-gray-200 text-base lg:text-lg lg:leading-8 tracking-wide mt-6 w-11/12">${tin.content}</p>
+                        <div class="w-full flex justify-end" >
+                            <button class="focus:outline-none focus:ring-2 ring-offset-2 focus:ring-gray-600 hover:opacity-75 mt-4 justify-end flex items-center cursor-pointer">
+                                <span class=" text-base tracking-wide text-indigo-700">Read more</span>
+                               <img class="ml-3 lg:ml-6" src="https://tuk-cdn.s3.amazonaws.com/can-uploader/2-column-with-main-and-supporting-svg1.svg" alt="arrow"/>
+                            </button>
+                        </div>
+                        <div class="h-5 w-2"></div>
+                    </div>
+                </div>
+                <div>
+                    <div class="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 gap-8">
+                        <div tabindex="0" class="focus:outline-none " aria-label="card 2" >
+                            <img tabindex="0" role="img" aria-label="gaming" class="focus:outline-none w-full" src="https://cdn.tuk.dev/assets/components/111220/Blg-6/blog(2).png" alt="games" />
+                            <div class="py-2 px-4 w-full flex justify-between bg-indigo-700">
+                                <p tabindex="0" class="focus:outline-none  text-sm text-white font-semibold tracking-wide">Bruce Wayne</p>
+                                <p tabindex="0" class="focus:outline-none text-sm text-white font-semibold tracking-wide">13TH Oct, 2020</p>
+                            </div>
+                            <div class="bg-white dark:bg-gray-800 px-3 lg:px-6 py-4 rounded-bl-3xl rounded-br-3xl">
+                                <h1 tabindex="0" class="focus:outline-none text-lg text-gray-900 dark:text-white font-semibold tracking-wider">${tin.title}</h1>
+                                <p tabindex="0" class="focus:outline-none text-gray-700 dark:text-gray-200 text-sm lg:text-base lg:leading-8 pr-4 tracking-wide mt-2">${tin.content}...</p>
+                            </div>
+                        </div>
+                        <div  tabindex="0" class="focus:outline-none" aria-label="card 3">
+                            <img tabindex="0" role="img" aria-label="gaming" class="focus:outline-none focus:outline-none w-full" src="https://cdn.tuk.dev/assets/components/111220/Blg-6/blog(3).png" alt="notes" />
+                            <div class="py-2 px-4 w-full flex justify-between bg-indigo-700">
+                                <p tabindex="0" class="focus:outline-none text-sm text-white font-semibold tracking-wide">Bruce Wayne</p>
+                                <p tabindex="0" class="focus:outline-none text-sm text-white font-semibold tracking-wide">13TH Oct, 2020</p>
+                            </div>
+                            <div class="bg-white dark:bg-gray-800 px-3 lg:px-6 py-4 rounded-bl-3xl rounded-br-3xl">
+                                <h1 tabindex="0" class="focus:outline-none text-lg text-gray-900 dark:text-white font-semibold tracking-wider">${tin.title}</h1>
+                                <p tabindex="0" class="focus:outline-none text-gray-700 dark:text-gray-200 text-sm lg:text-base lg:leading-8 pr-4 tracking-wide mt-2">${tin.content}...</p>
+                            </div>
+                        </div>
+                    </div>
 
-    <div class="flex lg:flex-row flex-col justify-between gap-8 pt-12">
-        <div class="w-full lg:w-5/12 flex flex-col justify-center">
-            <h1 class="text-3xl lg:text-4xl font-bold leading-9 text-gray-800 dark:text-white pb-4">Our Story</h1>
-            <p class="font-normal text-base leading-6 text-gray-600 dark:text-white">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum.In the first place we have granted to God, and by this our present charter confirmed for us and our heirs forever that the English Church shall be free, and shall have her rights entire, and her liberties inviolate; and we will that it be thus observed; which is apparent from</p>
-        </div>
-        <div class="w-full lg:w-8/12 lg:pt-8">
-            <div class="grid md:grid-cols-4 sm:grid-cols-2 grid-cols-1 lg:gap-4 shadow-lg rounded-md">
-                <div class="p-4 pb-6 flex justify-center flex-col items-center">
-                    <img class="md:block hidden" src="https://i.ibb.co/FYTKDG6/Rectangle-118-2.png" alt="Alexa featured Image" />
-                    <img class="md:hidden block" src="https://i.ibb.co/zHjXqg4/Rectangle-118.png" alt="Alexa featured Image" />
-                    <p class="font-medium text-xl leading-5 text-gray-800 dark:text-white mt-4">Alexa</p>
-                </div>
-                <div class="p-4 pb-6 flex justify-center flex-col items-center">
-                    <img class="md:block hidden" src="https://i.ibb.co/fGmxhVy/Rectangle-119.png" alt="Olivia featured Image" />
-                    <img class="md:hidden block" src="https://i.ibb.co/NrWKJ1M/Rectangle-119.png" alt="Olivia featured Image" />
-                    <p class="font-medium text-xl leading-5 text-gray-800 dark:text-white mt-4">Olivia</p>
-                </div>
-                <div class="p-4 pb-6 flex justify-center flex-col items-center">
-                    <img class="md:block hidden" src="https://i.ibb.co/Pc6XVVC/Rectangle-120.png" alt="Liam featued Image" />
-                    <img class="md:hidden block" src="https://i.ibb.co/C5MMBcs/Rectangle-120.png" alt="Liam featued Image" />
-                    <p class="font-medium text-xl leading-5 text-gray-800 dark:text-white mt-4">Liam</p>
-                </div>
-                <div class="p-4 pb-6 flex justify-center flex-col items-center">
-                    <img class="md:block hidden" src="https://i.ibb.co/7nSJPXQ/Rectangle-121.png" alt="Elijah featured image" />
-                    <img class="md:hidden block" src="https://i.ibb.co/ThZBWxH/Rectangle-121.png" alt="Elijah featured image" />
-                    <p class="font-medium text-xl leading-5 text-gray-800 dark:text-white mt-4">Elijah</p>
+                    <div class="mt-10 grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 gap-8">
+                        <div tabindex="0" class="focus:outline-none " aria-label="card 4">
+                            <img tabindex="0" role="img" aria-label="laptop" class="focus:outline-none w-full" src="https://cdn.tuk.dev/assets/components/111220/Blg-6/blog(4).png" alt="laptop" />
+                            <div class="py-2 px-4 w-full flex justify-between bg-indigo-700">
+                                <p tabindex="0" class="focus:outline-none text-sm text-white font-semibold tracking-wide">Bruce Wayne</p>
+                                <p tabindex="0" class="focus:outline-none text-sm text-white font-semibold tracking-wide">13TH Oct, 2020</p>
+                            </div>
+                            <div class="bg-white dark:bg-gray-800 px-3 lg:px-6 py-4 rounded-bl-3xl rounded-br-3xl">
+                                <h1 tabindex="0" class="focus:outline-none text-lg text-gray-900 dark:text-white font-semibold tracking-wider">${tin.title}</h1>
+                                <p tabindex="0" class="focus:outline-none text-gray-700 dark:text-gray-200 text-sm lg:text-base lg:leading-8 pr-4 tracking-wide mt-2">${tin.content}...</p>
+                            </div>
+                        </div>
+                        <div tabindex="0" class="focus:outline-none" aria-label="card 5">
+                            <img tabindex="0" role="img" aria-label="worker" class="focus:outline-none w-full" src="https://cdn.tuk.dev/assets/components/111220/Blg-6/blog(5).png" alt="worker" />
+                            <div class="py-2 px-4 w-full flex justify-between bg-indigo-700">
+                                <p tabindex="0" class="focus:outline-none text-sm text-white font-semibold tracking-wide">Bruce Wayne</p>
+                                <p tabindex="0" class="focus:outline-none text-sm text-white font-semibold tracking-wide">13TH Oct, 2020</p>
+                            </div>
+                            <div class="bg-white dark:bg-gray-800 px-3 lg:px-6 py-4 rounded-bl-3xl rounded-br-3xl">
+                                <h1 tabindex="0" class="focus:outline-none  text-lg text-gray-900 dark:text-white font-semibold tracking-wider">${tin.title}</h1>
+                                <p tabindex="0" class="focus:outline-none  text-gray-700 dark:text-gray-200 text-sm lg:text-base lg:leading-8 pr-4 tracking-wide mt-2 break-words">${tin.content}...</p>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
-
-    
-    
+    </div>`
+    ))
+}
+</div>  
     `
 }
 export default About
