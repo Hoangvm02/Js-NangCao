7a390256ddf531dfd4b50d2b16d94b7419d08ea6
Hoangvm02 <hoangvmph13792@fpt.edu.vn>
2022-05-24 16:10:30 +0700

    html-lab2

--- src/main.js
+++ src/main.js
@@ -1,20 +1,40 @@
+import Navigo from "navigo"
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+// import bootstrap
 import 'bootstrap/dist/css/bootstrap.min.css';
-function render () {
-    document.querySelector('#header').innerHTML = '<button class="btn btn-primary">Test</button>';
-    // document.querySelector('#header').innerHTML = Header.render();
-    document.querySelector('#content').innerHTML = '<div>CONTENT</div>';
+import Home from "./pages/Home";
+import About from "./pages/About";
+
+// Khởi tạo đối tượng navigo
+const router = new Navigo('/', {linksSelector: "a"})
+function render (content) {
+    // test boostrap button
+    // document.querySelector('#header').innerHTML = '<button class="btn btn-primary">Test</button>';
+
+    document.querySelector('#header').innerHTML = Header.render();
+    document.querySelector('#content').innerHTML = content;
     document.querySelector('#footer').innerHTML = Footer.render();
 }
 
+router.on({
+    "/": () => render(Home.render()),
+    "/about": () => render(About.render()),
+    "/news": () => render()
+});
+
+
+
+router.resolve();
+
+
     // arrow function : const tên Hàm = () => {};
 
-const arrowRender = () =>{
-    document.querySelector('#header').innerHTML = '<div>HEADER</div>';
-    document.querySelector('#content').innerHTML = '<div>CONTENT</div>';
-    document.querySelector('#footer').innerHTML = '<div>FOOTER</div>';
-}
+// const arrowRender = () =>{
+//     document.querySelector('#header').innerHTML = '<div>HEADER</div>';
+//     document.querySelector('#content').innerHTML = '<div>CONTENT</div>';
+//     document.querySelector('#footer').innerHTML = '<div>FOOTER</div>';
+// }
 
 
 // function sum(a,b){
@@ -27,4 +47,4 @@ const arrowRender = () =>{
 
 // const display = a => console.log(a); //Nếu chỉ có 1 tham số 
 
-render();
+// render();
